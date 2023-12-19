907f0800e54a1445aad33059c9e1d2ca7c59567a
kimyu0218 <serena35@ewhain.net>
2023-12-19 14:54:33 +0900

    feat: api description

---
+++ src/common/decorators/swagger/constants.ts
@@ -0,0 +1,9 @@
+export const API_DESCRIPTION = {
+  200: 'OK',
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Not Found',
+  409: 'Conflict',
+  500: 'Internal Server Error',
+};
