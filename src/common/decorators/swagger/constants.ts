ee4299c922fad1d3813de7a6672e92b29a117742
kimyu0218 <serena35@ewhain.net>
2023-12-19 19:10:47 +0900

    remove: delete unused constants
    decide to use constants of http module

--- src/common/decorators/swagger/constants.ts
+++
@@ -1,9 +0,0 @@
-export const API_DESCRIPTION = {
-  200: 'OK',
-  400: 'Bad Request',
-  401: 'Unauthorized',
-  403: 'Forbidden',
-  404: 'Not Found',
-  409: 'Conflict',
-  500: 'Internal Server Error',
-};
