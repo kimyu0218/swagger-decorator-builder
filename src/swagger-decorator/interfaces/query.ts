e6e834dff85bc4126994dc48862b086c9346f0a3
kimyu0218 <serena35@ewhain.net>
2024-01-13 23:46:23 +0900

    feat: SwaggerQuery 인터페이스 작성
    - nestjs/swagger의 api-query.decorator.ts 참고
    - ApiQueryOptions를 위한 인터페이스

---
+++ src/swagger-decorator/interfaces/query.ts
@@ -0,0 +1,33 @@
+/**
+ * https://github.com/nestjs/swagger/blob/master/lib/decorators/api-query.decorator.ts
+ * https://github.com/nestjs/swagger/blob/master/lib/interfaces/open-api-spec.interface.ts#L122
+ */
+import {
+  ContentObject,
+  ExampleObject,
+  ParameterStyle,
+  ReferenceObject,
+  SchemaObject,
+} from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+import { SwaggerEnumType } from '@nestjs/swagger/dist/types/swagger-enum.type';
+import { SwaggerType } from '../types';
+
+export interface SwaggerQuery {
+  allowEmptyValue?: boolean;
+  allowReserved?: boolean;
+  content?: ContentObject;
+  description?: string;
+  deprecated?: boolean;
+  enum?: SwaggerEnumType;
+  enumName?: string;
+  example?: any;
+  examples?: Record<string, ExampleObject | ReferenceObject>;
+  explode?: boolean;
+  format?: string;
+  isArray?: boolean;
+  name: string;
+  required?: boolean;
+  schema: SchemaObject;
+  style?: ParameterStyle;
+  type?: SwaggerType;
+}
